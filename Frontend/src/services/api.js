d08667de01c35592e02d6a90a9ea716efc6dab0b
Furkan Emre Saygın <furkanemresaygin@gmail.com>
2026-03-28 17:32:12 +0300

    react

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5012/api";
+const API_BASE_URL = "http://makerapi.runasp.net/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
